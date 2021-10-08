202142d8b0dc3453fc3e8e18ebaffbc5b74fdb85
Catherine Jarocki <cjarocki@gmail.com>
2021-10-07 22:52:45 -0500

    saved from shelf

--- src/main.js
+++ src/main.js
@@ -1,32 +1,24 @@
-const baseUrl = "https://api.unsplash.com/search/photos/?";
-// console.log(process.env.CLIENT_KEY);
-
 let clientKey = "NR1uPmeGJFvBROFnXHhupA30iwHO-Uu4cfRi9haU5PA";
-let unsplashQuery = "puppies";
+let searchQuery;
+const requestOptions = { method: 'GET', redirect: 'follow' };
 
-
-let requestUrl = `${baseUrl}client_id=${clientKey}&query=${unsplashQuery}`;
-
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-};
+function constructQueryUrl(clientKey, queryTerm) {
+    const baseUrl = "https://api.unsplash.com/search/photos/?";
+    return `${baseUrl}client_id=${clientKey}&query=${queryTerm}`;
+}
 
 function constructPhotoCreditUrl(incomingJson) {
-    var unsplashCreds = incomingJson["user"]["name"];
-    var unsplashUserName = incomingJson["user"]["username"];
+    const unsplashCreds = incomingJson["user"]["name"];
+    const unsplashUserName = incomingJson["user"]["username"];
     return `<p>Photo by <a href='https://unsplash.com/@${unsplashUserName}'>${unsplashCreds}</a> 
         on <a href="https://unsplash.com/?utm_source=kitten-coding-challenge&utm_medium=referral">Unsplash</a></p>`;
 }
 
-function createImageElement(imageData) {
-    const image = document.createElement("img");
-    image.src = imageData["urls"]["raw"];
-    image.alt = imageData["alt_description"];
-    return image;
+function sendQuery() {
+    searchQuery = document.getElementById("searchImages").value;
+    let queryUrl = constructQueryUrl(clientKey, searchQuery);
+    fetchUnsplash(queryUrl, requestOptions);
 }
-
-// perform new query? argh
 
 async function fetchUnsplash(url, options){
     return fetch(url, options)
@@ -34,25 +26,25 @@ async function fetchUnsplash(url, options){
             if(response.ok) {
                 response.json().then(function(data) {
                     let results = data["results"];
-                    console.log(results)
+                    const targetElem = document.getElementById("fetchedUnsplashSrc");
+                    document.getElementById("queryTerm").innerHTML = searchQuery;
+
                     for(let i = 0; i < results.length; i++) {
-                        var image = createImageElement(data["results"][i]);
-                        document.getElementById("fetchedUnsplashSrc").appendChild(image);
-                        document.getElementById("unsplashImageLink").href += results[i]["urls"]["full"];
-                        // document.getElementById("unsplashImageLink").appendChild(image);
-                        document.getElementById("fetchedUnsplashSrc").innerHTML += constructPhotoCreditUrl(data["results"][i]);
+                        targetElem.insertAdjacentHTML("beforeend",
+                            `<div class="queryImage">
+                                <a href="${results[i]['urls']['full']}">
+                                    <img src="${results[i]['urls']['raw']}" alt="${results[i]['alt_description']}">
+                                </a>
+                                ${constructPhotoCreditUrl(results[i])}
+                            </div>`
+                        );
                     }
-
-                    var queryName = document.getElementById('queryTerm');
-                    queryName.innerHTML = unsplashQuery;
-
                 });
             } else {
                 console.log("response failed :/ uwu");
             }
-        } )
-        .catch(error => console.log('error', error)
-        );
+        })
+        .catch(error => console.log('Error:', error));
 }
 
-fetchUnsplash(requestUrl, requestOptions);
+sendQuery();
